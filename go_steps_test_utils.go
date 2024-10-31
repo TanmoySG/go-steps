67eafeeb6df3ea19c0f584e77049abbf9370cc7a
Tanmoy Sen Gupta <tanmoysps@gmail.com>
2024-10-31 15:46:36 +0530

    add tests

---
+++ go_steps_test_utils.go
@@ -0,0 +1,13 @@
+package gosteps
+
+var (
+	stepError1 = StepError{
+		StepErrorNameOrId: "error1",
+		StepErrorMessage:  "error1",
+	}
+
+	stepError2 = StepError{
+		StepErrorNameOrId: "error2",
+		StepErrorMessage:  "error2",
+	}
+)
